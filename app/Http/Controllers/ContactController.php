1c8f5859622919f8c1f3652452e4e1aa73125027
Muhammad Ahad <muhammadahad9615@gmail.com>
2020-05-16 23:56:12 +0500

    added update operation

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -76,7 +76,8 @@ class ContactController extends Controller
      */
     public function edit($id)
     {
-        //
+        $contact = Contact::find($id);
+        return view('contacts.edit', compact('contact'));
     }
 
     /**
@@ -88,7 +89,22 @@ class ContactController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required'
+        ]);
+
+        $contact = Contact::find($id);
+        $contact->first_name =  $request->get('first_name');
+        $contact->last_name = $request->get('last_name');
+        $contact->email = $request->get('email');
+        $contact->job_title = $request->get('job_title');
+        $contact->city = $request->get('city');
+        $contact->country = $request->get('country');
+        $contact->save();
+
+        return redirect('/contacts')->with('success', 'Contact Updated!');
     }
 
     /**
